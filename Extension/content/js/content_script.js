6fba3f65dea5f7073756dd3f84cfe4926e05ce80
rob_green <trossr32@gmail.com>
2020-05-06 20:56:22 +0100

    decode url component when populating search field and simplify search value retrieval

--- Extension/content/js/content_script.js
+++ Extension/content/js/content_script.js
@@ -15,17 +15,17 @@ var init = function (settings) {
             console.log('site', site);
 
             if (window.location.href.includes(site.domain)) {
-                var search = window.location.href.match(/(?<!\?.+)(?<=\/)[\s\w-]+(=[/\r\n?]|$)/g);
+                var search = window.location.href.replace(/(.+\/)/g, '');
 
                 console.log('search', search);
 
-                if (search.length && search.length > 0 && search[0].trim() !== '') {
+                if (search.trim() !== '') {
                     // use jquery selector and then retrieve the DOM element
                     var searchInput = $(site.searchInputSelector)[0];
 
                     if (searchInput) {
                         // jquery can't be used to trigger the input event here so rely on vanilla js for event triggering
-                        searchInput.value = search[0].trim();
+                        searchInput.value = decodeURIComponent(search.trim());
 
                         var event = document.createEvent('Event');
                         event.initEvent('input', true, true);
