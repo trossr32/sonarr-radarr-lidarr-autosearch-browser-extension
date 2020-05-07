02ff9690ca9ce26c0616f6cfbcd8de66a6ef5975
rob_green <trossr32@gmail.com>
2020-05-07 12:39:57 +0100

    fix deployment script

--- CreatePackage.ps1
+++ CreatePackage.ps1
@@ -1,5 +1,5 @@
 $extension = Resolve-Path -LiteralPath "Extension\"
-$zip = Resolve-Path -LiteralPath "sonarr-radarr-lidarr-autosearch-chrome-extension.zip"
+$zip = Join-Path (Get-Location).Path "sonarr-radarr-lidarr-autosearch-chromium-extension.zip"
 
 print
 
