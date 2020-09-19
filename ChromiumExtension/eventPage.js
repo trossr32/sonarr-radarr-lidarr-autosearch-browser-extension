c2819213caef9afcbc404f60f2f32524c2fc00fc
Rob Green <rob@robgreen.me>
2020-09-19 19:00:11 +0100

    fixes #5 fixes #6
    fixed search failing on invalid input by replacing periods with spaces and encoding the search term.
    fixed auto search kicking off when no search term is present in the url.

--- ChromiumExtension/eventPage.js
+++ ChromiumExtension/eventPage.js
@@ -68,7 +68,7 @@ function onClickHandler(info, tab) {
         for (var i = 0; i < settings.sites.length; i++) {
             if (info.menuItemId == (settings.sites[i].id + 'Menu')) {
                 chrome.tabs.create({
-                    'url': settings.sites[i].domain.replace(/\/$/, '') + settings.sites[i].searchPath + info.selectionText
+                    'url': settings.sites[i].domain.replace(/\/$/, '') + settings.sites[i].searchPath + encodeURIComponent(info.selectionText).replace(/\./g, ' ')
                 });
             }
         }
