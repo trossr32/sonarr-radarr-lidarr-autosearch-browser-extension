dd3287ef8f69c74ad52e44b6544e44291232c3f3
Rob Green <rob@robgreen.me>
2024-04-29 00:06:14 +0100

    add schedule, extend expect timeout

--- tests/playwright/playwright.config.ts
+++ tests/playwright/playwright.config.ts
@@ -11,6 +11,9 @@ import { defineConfig, devices } from '@playwright/test';
  */
 export default defineConfig({
   testDir: './site.integrations.tests',
+  expect: {
+    timeout: 10000,
+  },
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
