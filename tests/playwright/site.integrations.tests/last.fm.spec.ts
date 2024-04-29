8cfdc7e79417254b3fd00d9e456ca7660ede264b
Rob Green <rob@robgreen.me>
2024-04-29 09:33:19 +0100

    last.fm test now working

--- tests/playwright/site.integrations.tests/last.fm.spec.ts
+++ tests/playwright/site.integrations.tests/last.fm.spec.ts
@@ -2,10 +2,8 @@ import { iconDataLocator } from '../constants';
 import { test, expect } from '../fixtures';
 import { getExpectedLidarrUrl } from '../helpers';
 
-// TODO: test is not currently working but the integration is. Not sure why
-
-// test('last.fm artist has lidarr icon', async ({ page }) => {
-//   await page.goto('https://www.last.fm/music/Aqua', { waitUntil: 'commit' });
-//   await expect(page.locator(iconDataLocator)).toHaveCount(1);
-//   await expect(page.locator(iconDataLocator)).toHaveAttribute('href', getExpectedLidarrUrl('Aqua'));
-// });
+test('last.fm artist has lidarr icon', async ({ page }) => {
+  await page.goto('https://www.last.fm/music/Aqua', { waitUntil: 'commit' });
+  await expect(page.locator(iconDataLocator)).toHaveCount(1);
+  await expect(page.locator(iconDataLocator)).toHaveAttribute('href', getExpectedLidarrUrl('Aqua'));
+});
