9b74477a93c733e227d0e514a990374b808dbc38
Rob Green <rob@robgreen.me>
2025-09-21 16:20:22 +0100

    Add Sonarr icon test for Letterboxd movies
    Introduces a new Playwright test to verify the presence and correct href of the Sonarr icon on a Letterboxd movie page. Also updates the Radarr icon test to use the IMDb ID instead of TMDb ID for consistency.

--- tests/playwright/site.integrations.tests/letterboxd.spec.ts
+++ tests/playwright/site.integrations.tests/letterboxd.spec.ts
@@ -1,9 +1,15 @@
 import { iconDataLocator } from '../constants';
 import { test, expect } from '../fixtures';
-import { getExpectedRadarrUrl } from '../helpers';
+import { getExpectedRadarrUrl, getExpectedSonarrUrl } from '../helpers';
 
 test('letterboxd movie has radarr icon', async ({ page }) => {
   await page.goto('https://letterboxd.com/film/the-dark-knight/', { waitUntil: 'commit' });
   await expect(page.locator(iconDataLocator)).toHaveCount(1);
-  await expect(page.locator(iconDataLocator)).toHaveAttribute('href', getExpectedRadarrUrl('tmdb:155'), { ignoreCase: true });
+  await expect(page.locator(iconDataLocator)).toHaveAttribute('href', getExpectedRadarrUrl('imdb:tt0468569'), { ignoreCase: true });
+});
+
+test('letterboxd movie has sonarr icon', async ({ page }) => {
+  await page.goto('https://letterboxd.com/film/chernobyl/', { waitUntil: 'commit' });
+  await expect(page.locator(iconDataLocator)).toHaveCount(1);
+  await expect(page.locator(iconDataLocator)).toHaveAttribute('href', getExpectedSonarrUrl('imdb:tt7366338'), { ignoreCase: true });
 });
